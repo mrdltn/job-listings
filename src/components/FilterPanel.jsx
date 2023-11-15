b5791bb7532195be5bef652dca61e83b0c251912
mrdltn <mrdltn@hotmail.com>
2023-11-15 17:22:30 +0700

    filtration development

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,18 +1,38 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { selectFilters } from '../store/filters/filter-selectors';
+import { removeFilter, clearFilter } from '../store/filters/filter-actions';
+
 import { Badge } from '../UI/Badge';
 import { Stack } from '../UI/Stack';
 import { Card } from '../UI/Card';
 
 const FilterPanel = () => {
+    const dispatch = useDispatch();
+
+    const currentFilters = useSelector(selectFilters);
+
+    if (currentFilters.length === 0) {
+        return null;
+    }
+
     return (
         <Card className='filter-panel'>
             <div className='filter-panel-wrapper'>
                 <Stack>
-                    <Badge variant='clearable'>Frontend</Badge>
-                    <Badge variant='clearable'>Backend</Badge>
-                    <Badge variant='clearable'>React</Badge>
+                    {currentFilters.map((filter) => (
+                        <Badge
+                            key={filter}
+                            variant='clearable'
+                            onClear={() => dispatch(removeFilter(filter))}
+                        >
+                            {filter}
+                        </Badge>
+                    ))}
                 </Stack>
 
-                <button className='link'>Clear</button>
+                <button className='link' onClick={() => dispatch(clearFilter)}>
+                    Clear
+                </button>
             </div>
         </Card>
     );
--- src/store/positions/position-selectors.js
+++ src/store/positions/position-selectors.js
@@ -1,15 +1,15 @@
 export const selectAllPositions = (state) => state.positions;
 
 export const selectVisiblePositions = (state, filters = []) => {
-    if ((filters.length = 0)) {
-        return state.positions.filter((pos) => {
-            const posFilters = [].concat(
-                pos.role,
-                pos.level,
-                ...pos.languages,
-                ...pos.tools
-            );
-            return filters.every((filter) => posFilters.includes(filter));
-        });
-    }
+    if (filters.length === 0) return state.positions;
+
+    return state.positions.filter((pos) => {
+        const posFilters = [].concat(
+            pos.role,
+            pos.level,
+            ...pos.languages,
+            ...pos.tools
+        );
+        return filters.every((filter) => posFilters.includes(filter));
+    });
 };
