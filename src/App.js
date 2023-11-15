f1a0ce93e8879702865269ecd2e0171ca79beff5
mrdltn <mrdltn@hotmail.com>
2023-11-15 11:49:56 +0700

    add Provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import data from './mock/data.json';
+import { addPositions } from './store/positions/position-actions';
+
 import { FilterPanel } from './components/FilterPanel';
 import { JobList } from './components/JobList';
 import { Header } from './components/Header';
 
 function App() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(addPositions(data));
+    });
+
     return (
         <>
             <Header />
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { rootReducer } from './root-reducer';
+
+// import { createStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export { store };
